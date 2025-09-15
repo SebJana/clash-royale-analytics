57cdad6d95e8523e2ad5bace1837e8788d02eef3
Sebastian Jana <s.jana@oth-aw.de>
2025-09-15 17:52:58 +0200

    refactor: rework the battle header display

--- frontend/src/components/battle/battle.tsx
+++ frontend/src/components/battle/battle.tsx
@@ -2,6 +2,7 @@ import { memo } from "react";
 import { DeckComponent } from "../deck/deck";
 import type { Battle } from "../../types/lastBattles";
 import type { CardMeta } from "../../types/cards";
+import { Crown } from "lucide-react";
 import { datetimeToLocale } from "../../utils/datetimeToLocale";
 import "./battle.css";
 
@@ -12,15 +13,46 @@ export const BattleComponent = memo(function BattleComponent({
   battle: Battle;
   cards: CardMeta[];
 }>) {
+  // Map the color of the result
+  const getResultColor = (result: string) => {
+    switch (result.toLowerCase()) {
+      case "victory":
+        return "blue";
+      case "defeat":
+        return "red";
+      case "draw":
+        return "gray";
+      default:
+        return "gray";
+    }
+  };
+
   return (
     <div className="battle-component-container">
-      <h2>
-        {battle.gameMode} — {datetimeToLocale(battle.battleTime)} —{" "}
-        {battle.gameResult}
-      </h2>
+      <div className="battle-component-header">
+        <div className="battle-component-header-left">
+          <div
+            className={`battle-component-header-result battle-component-result-${getResultColor(
+              battle.gameResult
+            )}`}
+          >
+            {battle.gameResult}
+          </div>
+          <span className="battle-component-battle-time">
+            {datetimeToLocale(battle.battleTime)}
+          </span>
+        </div>
+        <h2 className="battle-component-game-mode">{battle.gameMode}</h2>
+        <div className="battle-component-score">
+          <Crown className="battle-component-crown battle-component-team-crown battle-component-crown-blue" />
+          <span className="battle-component-score-text">
+            {battle.team[0].crowns} - {battle.opponent[0].crowns}
+          </span>
+          <Crown className="battle-component-crown battle-component-opponent-crown battle-component-crown-red" />
+        </div>
+      </div>
       <div className="battle-component-decks">
         <div className="battle-component-col battle-component-team">
-          <h3 className="battle-component-heading">Team</h3>
           {battle.team?.map((t, i) => (
             <section
               key={`${battle.battleTime}-team-${t.tag ?? i}`}
@@ -35,7 +67,6 @@ export const BattleComponent = memo(function BattleComponent({
         </div>
 
         <div className="battle-component-col battle-component-opponent">
-          <h3 className="battle-component-heading">Opponent</h3>
           {battle.opponent?.map((o, i) => (
             <section
               key={`${battle.battleTime}-opp-${o.tag ?? i}`}
